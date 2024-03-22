513b65adc9c709df0457b0bb584822897dd46f10
Dragan <milosevicdragan002@gmail.com>
2024-03-22 14:43:35 +0100

    Create user class

---
+++ HCI_Project/Model/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI_Project.Model
+{
+    public enum UserRole
+    {
+        Admin,
+        Visitor
+    }
+    [Serializable]
+    public class User
+    {
+        private string username;
+        private string password;
+        private UserRole role;
+
+        public User(string username, string password, UserRole role)
+        {
+            this.username = username;
+            this.password = password;
+            this.role = role;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public UserRole Role { get => role; set => role = value; }
+
+
+    }
+}
