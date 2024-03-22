b0e6705aee7d84d0a3454d42af3d63da378a71a1
Dragan <milosevicdragan002@gmail.com>
2024-03-22 15:40:36 +0100

    Login button click

--- HCI_Project/MainWindow.xaml.cs
+++ HCI_Project/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HCI_Project.Helpers;
+using HCI_Project.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,24 +22,60 @@ namespace HCI_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DataIO serializer = new DataIO();
+        public List<User>Users = new List<User>();
+        public List<User> users = new List<User>();
         public MainWindow()
         {
             InitializeComponent();
+            User visitor = new User("visitor", "visitor123", UserRole.Visitor);
+            User admin = new User("admin", "admin123", UserRole.Admin);
+            users.Add(visitor);
+            users.Add(admin);   
+            serializer.SerializeObject<List<User>>(users,"Users.xaml");
+            Users = serializer.DeSerializeObject<List<User>>("Users.xaml");
+
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
+            foreach (User user in users)
+            {
+                if (user.Username == UsernameTextBox.Text && user.Password == PasswordBox.Password)
+                {
+                    UsernameErrorLabel.Content = string.Empty;
+                    UsernameErrorLabel.BorderBrush = Brushes.Gray;
+                    PasswordErrorLabel.Content = string.Empty;
+                    PasswordErrorLabel.BorderBrush = Brushes.Gray;
+                    if (user.Role == UserRole.Admin)
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+                    UsernameErrorLabel.Content = "Wrong input";
+                    UsernameTextBox.BorderBrush = Brushes.Red;
+                    PasswordErrorLabel.Content = "Wrong input";
+                    PasswordErrorLabel.BorderBrush = Brushes.Red;
+                }
+            }
 
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
+
     }
 }
