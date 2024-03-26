b5ddfe0fef65cf3b7efb64978ce16a5374b5ebd8
Dragan <milosevicdragan002@gmail.com>
2024-03-26 10:29:53 +0100

    Change Page with window

---
+++ HCI_Project/UserWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using HCI_Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HCI_Project
+{
+    /// <summary>
+    /// Interaction logic for UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        public UserWindow(User user)
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddPlatformWindow addPlatformWindow = new AddPlatformWindow();
+            addPlatformWindow.ShowDialog();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
