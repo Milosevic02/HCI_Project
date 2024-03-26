b6ca48a97ada6208d13ecb7ec43f00e6a56e22d5
Dragan <milosevicdragan002@gmail.com>
2024-03-26 10:32:30 +0100

    Change Page for window

---
+++ HCI_Project/AddPlatformWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HCI_Project
+{
+    /// <summary>
+    /// Interaction logic for AddPlatformWindow.xaml
+    /// </summary>
+    public partial class AddPlatformWindow : Window
+    {
+        public AddPlatformWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void KOTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void KOTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void KOTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        private void NameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditorRichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditorRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void FontSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
