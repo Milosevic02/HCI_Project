b11dc6566f648fe8be3cde9e8c88fc2e9b31a538
Dragan <milosevicdragan002@gmail.com>
2024-03-26 10:32:37 +0100

    Update ToastNotification.cs

--- HCI_Project/Helpers/ToastNotification.cs
+++ HCI_Project/Helpers/ToastNotification.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Notification.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,20 +10,20 @@ namespace HCI_Project.Helpers
 {
     public class ToastNotification
     {
-        //public string Title { get; set; }
-        //public string Message { get; set; }
-        //public NotificationType Type { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public NotificationType Type { get; set; }
 
-        //public ToastNotification()
-        //{
+        public ToastNotification()
+        {
 
-        //}
+        }
 
-        //public ToastNotification(string title, string message, NotificationType type)
-        //{
-        //    Title = title;
-        //    Message = message;
-        //    Type = type;
-        //}
+        public ToastNotification(string title, string message, NotificationType type)
+        {
+            Title = title;
+            Message = message;
+            Type = type;
+        }
     }
 }
